b0e606364a19a533db8fd99fcaa6d91570897be6
samane Tohidi <s.tohidi39@gmail.com>
2021-06-17 17:30:35 +0430

    Complete the EpubViewer page code

--- app/src/main/java/com/amorphteam/ketub/ui/epub/EpubViewer.kt
+++ app/src/main/java/com/amorphteam/ketub/ui/epub/EpubViewer.kt
@@ -1,12 +1,37 @@
 package com.amorphteam.ketub.ui.epub
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.ViewModelProvider
 import com.amorphteam.ketub.R
 
+
 class EpubViewer : AppCompatActivity() {
+
+    private lateinit var viewModel: EpubViewerViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_epub_viewer)
+
+        viewModel = ViewModelProvider(this).get(EpubViewerViewModel::class.java)
+
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+        supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_back)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+        toolbar.setNavigationOnClickListener { finish() }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_epub, menu)
+        return true
     }
 }
---
+++ app/src/main/java/com/amorphteam/ketub/ui/epub/EpubViewerViewModel.kt
@@ -0,0 +1,10 @@
+package com.amorphteam.ketub.ui.epub
+
+import androidx.lifecycle.ViewModel
+
+class EpubViewerViewModel: ViewModel() {
+
+    init {
+
+    }
+}
