64df6e265fa1419e87b131f6314774ba0faf960d
samane Tohidi <s.tohidi39@gmail.com>
2023-04-18 20:22:57 +0330

    Set fragment index list second

--- app/src/main/java/com/amorphteam/ketub/ui/main/tabs/index/tabLayout/IndexListSecondFragment.kt
+++ app/src/main/java/com/amorphteam/ketub/ui/main/tabs/index/tabLayout/IndexListSecondFragment.kt
@@ -1,5 +1,6 @@
 package com.amorphteam.ketub.ui.main.tabs.index.tabLayout
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,27 +8,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.amorphteam.ketub.R
+import com.amorphteam.ketub.databinding.FragmentIndexListFirstBinding
 import com.amorphteam.ketub.databinding.FragmentIndexListSecondBinding
+import com.amorphteam.ketub.ui.epub.EpubViewer
+import com.amorphteam.ketub.ui.main.tabs.index.adapter.IndexClickListener
+import com.amorphteam.ketub.ui.main.tabs.index.adapter.RVIndexListAdapter
 
 class IndexListSecondFragment : Fragment() {
+
+    private lateinit var binding: FragmentIndexListSecondBinding
+    private lateinit var viewModel: IndexListSecondViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        var viewModel = ViewModelProvider(this)[IndexListSecondViewModel::class.java]
-
-        val binding = DataBindingUtil.inflate<FragmentIndexListSecondBinding>(
-            inflater,
-            R.layout.fragment_index_list_second,
-            container,
-            false
+        viewModel = ViewModelProvider(this)[IndexListSecondViewModel::class.java]
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_index_list_second, container, false
         )
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        viewModel.startEpubAct.observe(viewLifecycleOwner) {
+            if (it) startActivity(Intent(activity, EpubViewer::class.java))
+        }
+
+        handleIndexRecyclerView()
 
         return binding.root
     }
 
+    private fun handleIndexRecyclerView() {
+
+        val index = RVIndexListAdapter(IndexClickListener {
+            viewModel.openEpubAct()
+        })
+
+        index.submitList(viewModel.getIndexList().value)
+        val layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = index
+    }
 
 }
--- app/src/main/java/com/amorphteam/ketub/ui/main/tabs/index/tabLayout/IndexListSecondViewModel.kt
+++ app/src/main/java/com/amorphteam/ketub/ui/main/tabs/index/tabLayout/IndexListSecondViewModel.kt
@@ -1,7 +1,19 @@
 package com.amorphteam.ketub.ui.main.tabs.index.tabLayout
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.amorphteam.ketub.ui.main.tabs.index.model.IndexModel
+import com.amorphteam.ketub.utility.TempData
 
 class IndexListSecondViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
+    var startEpubAct = MutableLiveData<Boolean>()
+
+    fun getIndexList(): MutableLiveData<ArrayList<IndexModel>> {
+        val array = MutableLiveData<ArrayList<IndexModel>>()
+        array.value = TempData.bookIndex
+        return array
+    }
+
+    fun openEpubAct() {
+        startEpubAct.value = true
+    }}
