d13c67742158c3c556cf06e8cd0d3b4b0d3e7be9
ali johar <johar.ali1@gmail.com>
2023-04-30 20:44:44 +0330

    Handle UI Add ViewModel Add Adapter

--- app/src/main/java/com/amorphteam/ketub/ui/epub/fragments/search/SearchSingleFragment.kt
+++ app/src/main/java/com/amorphteam/ketub/ui/epub/fragments/search/SearchSingleFragment.kt
@@ -1,5 +1,6 @@
 package com.amorphteam.ketub.ui.epub.fragments.search
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,8 +8,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.amorphteam.ketub.R
+import com.amorphteam.ketub.ui.epub.EpubActivity
 import com.amorphteam.ketub.ui.epub.fragments.toc.TocSingleViewModel
+import com.amorphteam.ketub.ui.search.adapter.SearchClickListener
+import com.amorphteam.ketub.ui.search.adapter.SearchListAdapter
 
 class SearchSingleFragment : Fragment() {
 
@@ -25,7 +30,24 @@ class SearchSingleFragment : Fragment() {
         )
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
+        viewModel.startEpubFrag.observe(requireActivity()) {
+//            if (it) startActivity(Intent(this, EpubActivity::class.java))
+        }
+        val adapter = SearchListAdapter(SearchClickListener { id ->
+            viewModel.openEpubFrag()
+        })
+
+        handleSearchResult(adapter)
+
+
         return binding.root
+    }
+
+    private fun handleSearchResult(adapter: SearchListAdapter) {
+        adapter.submitList(viewModel.getSearchList().value)
+        val layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = adapter
     }
 
 
--- app/src/main/java/com/amorphteam/ketub/ui/epub/fragments/search/SearchSingleViewModel.kt
+++ app/src/main/java/com/amorphteam/ketub/ui/epub/fragments/search/SearchSingleViewModel.kt
@@ -1,7 +1,24 @@
 package com.amorphteam.ketub.ui.epub.fragments.search
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.amorphteam.ketub.ui.search.model.SearchModel
+import com.amorphteam.ketub.utility.TempData
 
 class SearchSingleViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _startEpubFrag = MutableLiveData<Boolean>()
+    val startEpubFrag: LiveData<Boolean>
+        get() = _startEpubFrag
+
+
+    fun getSearchList(): MutableLiveData<ArrayList<SearchModel>> {
+        val array = MutableLiveData<ArrayList<SearchModel>>()
+        array.value = TempData.searchResult
+        return array
+    }
+
+    fun openEpubFrag() {
+        _startEpubFrag.value = true
+    }
 }
